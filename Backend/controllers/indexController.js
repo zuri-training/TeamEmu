f035f10e1be33d83ce46989cf5b0a425f5f141fb
Efezino <enduranceidisi13@gmail.com>
2022-12-13 07:31:32 +0100

    Revert "progress actual build"

--- Backend/controllers/indexController.js
+++ Backend/controllers/indexController.js
@@ -1,17 +1,5 @@
-exports.welcomePage = (req, res) => {res.render("index")}
+exports.welcomePage = (req, res) => {res.render("welcome")}
 
 exports.dashboard = (req, res) => {
 		res.render("dashboard", {user: req.user})
-}
-
-exports.editWebsite = (req, res) => {
-	res.render("editWebsite", {user: req.user})
-}
-
-exports.generalSettings = (req, res) => {
-	res.render("generalSettings")
-}
-
-exports.dashboardMedia = (req, res) => {
-	res.render("dashboardMedia")
 }
