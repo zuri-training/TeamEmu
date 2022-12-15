a003728708046040ffd4154412094b32478c5057
Ademuyiwa Joy <joyoluwatobi36@gmail.com>
2022-12-15 07:38:44 +0100

    Backend build

--- Backend/routes/indexRoute.js
+++ Backend/routes/indexRoute.js
@@ -1,11 +1,15 @@
 const router = require("express").Router()
 const {forwardAuthenticated, ensureAuthenticated} = require("../middleware/auth")
-const {welcomePage, dashboard, editWebsite, generalSettings, dashboardMedia, review} = require("../controllers/indexController")
+const {welcomePage, dashboard, editWebsite, 
+	generalSettings, dashboardMedia, review,
+	templates, websiteTypes} = require("../controllers/indexController")
 
 router.get("/", forwardAuthenticated, welcomePage)
 router.get("/dashboard", ensureAuthenticated, dashboard)
 router.get('/editWebsite', ensureAuthenticated, editWebsite )
 router.get('/generalSettings', ensureAuthenticated, generalSettings)
 router.get('/dashboardMedia', ensureAuthenticated, dashboardMedia)
+router.get('/templates', templates)
+router.get('/websiteType', websiteTypes)
 router.post('/review', review)
 module.exports = router;
